3c899a1a88ef0cffb540d38da873dce0e1da9b4d
msgirlperl <mspeert@gmail.com>
2018-06-14 21:04:24 -0400

    Cleaned up: fixed up instructions panel

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import todo from './primary-todo.svg';
+import React, { Component } from "react";
+import "./App.css";
+import todo from "./primary-todo.svg";
 
-import tasks from '../tasks'; // the data!
-import TaskGroupList from '../components/TaskGroupList';
-import TaskList from '../components/TaskList';
+import tasks from "../tasks"; // the data!
+import TaskGroupList from "../components/TaskGroupList";
+import TaskList from "../components/TaskList";
 
-import Log from '../Utils/Utils';
+import Log from "../Utils/Utils";
 
 class App extends Component {
-
   /* Iterates over the payload and creates groups for the components to consume.  Each entry in the array is a group of tasks.
   We also determine the "lock" status of each task.
 
@@ -36,12 +35,11 @@ class App extends Component {
 
   */
   buildGroups() {
-
     let groups = [];
 
     // determine lock status of each task
-    tasks.forEach((task) => {
-      if (task.dependencyIds.length === 0){
+    tasks.forEach(task => {
+      if (task.dependencyIds.length === 0) {
         task.isLocked = false;
       } else {
         task.isLocked = this.isLocked(task, this.findTaskInSource);
@@ -49,24 +47,23 @@ class App extends Component {
 
       // build up the groups with their corresponding task lists
       let group = groups.find(gp => gp.groupName === task.group);
-      if (group)
-      {
+      if (group) {
         group.groupTasks.push(task);
-      }
-      else {
+      } else {
         groups.push({
           groupName: task.group,
           groupTasks: [task]
-      })}
+        });
+      }
     });
 
     return groups;
-  };
+  }
 
   // sets the selected task group to display
-  onGroupClicked = (groupName) => {
-    this.setState({selectedTaskGroupName: groupName});
-  }
+  onGroupClicked = groupName => {
+    this.setState({ selectedTaskGroupName: groupName });
+  };
 
   /* Updates the given task's state with the following rules:
     1. If the task is locked, nothing happens
@@ -75,137 +72,243 @@ class App extends Component {
 
     Then, we re-evaluate the rest of the tasks statuses as a dependent task may have changed.
   */
-  onTaskClicked = (taskId) => {
+  onTaskClicked = taskId => {
     //TODO: Let's discuss whether we should just be updating the tasks every time
 
-    let taskGroup = this.state.taskGroups.find( gp => gp.groupName === this.state.selectedTaskGroupName);
-    let currentTask = taskGroup.groupTasks.find( t => t.id === taskId);
+    let taskGroup = this.state.taskGroups.find(
+      gp => gp.groupName === this.state.selectedTaskGroupName
+    );
+    let currentTask = taskGroup.groupTasks.find(t => t.id === taskId);
 
-    if (currentTask.isLocked){
+    if (currentTask.isLocked) {
       return false; // no changes allowed
     }
 
     // If the current task is NOT locked, toggle it's completion status.
-    if (!this.isLocked(currentTask, this.findTaskInState)){
-        if (currentTask.completedAt === null) {
-          currentTask.completedAt = Date.now();
-        } else {
-          currentTask.completedAt = null;
-        }
+    if (!this.isLocked(currentTask, this.findTaskInState)) {
+      if (currentTask.completedAt === null) {
+        currentTask.completedAt = Date.now();
+      } else {
+        currentTask.completedAt = null;
+      }
     }
 
     // Arrel, what's the best way to do this?  This can't be the industry standard.
     this.updateAllLockStatus(); // re-evaluate other tasks
     this.setState(this.state);
-  }
+  };
 
-    /*
+  /*
     *  Iterate over all the individual tasks and determine their current lock status in regards to task dependencies
     */
-    updateAllLockStatus = () => {
+  updateAllLockStatus = () => {
     //TODO: Consider for optimization.  For now, we will re-parse all the tasks and make sure they're up to date.
     // Perhaps we should maintain 2-way binding information (parents know of children too?)
 
-      let stateTasksGroups = this.state.taskGroups;
-      stateTasksGroups.forEach((group) => {
-          group.groupTasks.forEach((task) => {
-            task.isLocked = this.isLocked(task, this.findTaskInState.bind(this));
-          }, this);
+    let stateTasksGroups = this.state.taskGroups;
+    stateTasksGroups.forEach(group => {
+      group.groupTasks.forEach(task => {
+        task.isLocked = this.isLocked(task, this.findTaskInState.bind(this));
       }, this);
+    }, this);
 
-      this.setState({taskGroups:stateTasksGroups });
-  }
+    this.setState({ taskGroups: stateTasksGroups });
+  };
 
   // Crawls the dependency tree to determine if the task is locked or if it can be completed
   // findTask: callback to find a task (alternates depending on whether it's the source data (payload) or state)
   isLocked(task, findTask) {
     let parentIds = task.dependencyIds;
     // if the task has no dependencies, it is never locked
-    if (task.completedAt || parentIds.length === 0){
+    if (task.completedAt || parentIds.length === 0) {
       return false;
     }
 
     // if any parent is incomplete or locked, then this task is locked.
     let someParentLocked = parentIds.some(pId => {
-      let parentTask = findTask.call(this,pId);
-      if (!parentTask){
+      let parentTask = findTask.call(this, pId);
+      if (!parentTask) {
         return false; // ignore tasks that aren't found
       }
-      return (parentTask.completedAt === null) || this.isLocked(parentTask, findTask);
+      return (
+        parentTask.completedAt === null || this.isLocked(parentTask, findTask)
+      );
     });
 
     return someParentLocked;
   }
 
   // Iterates all task groups (in state) to find a given task (based on id) and retrieves it
-  findTaskInState(taskId){
+  findTaskInState(taskId) {
     let task = null;
-    this.state.taskGroups.forEach( taskGroup => {
-      let tempTask = taskGroup.groupTasks.find( t => t.id === taskId);
-      if(tempTask)
-        task = tempTask;
-    })
+    this.state.taskGroups.forEach(taskGroup => {
+      let tempTask = taskGroup.groupTasks.find(t => t.id === taskId);
+      if (tempTask) task = tempTask;
+    });
     return task;
-  };
+  }
 
   // Iterates all task groups (in source data) to find a given task (based on id) and retrieves it
-  findTaskInSource(taskId){
-    return tasks.find( t => t.id === taskId);
-  };
+  findTaskInSource(taskId) {
+    return tasks.find(t => t.id === taskId);
+  }
 
   constructor() {
-      super();
+    super();
 
-      const groups = this.buildGroups();
-      const groupName = groups[0].groupName; // on page load, let's default to first group
+    const groups = this.buildGroups();
+    const groupName = groups[0].groupName; // on page load, let's default to first group
 
-      this.state = {
-        taskGroups: groups,
-        selectedTaskGroupName:groupName // the name of the selected taskGroup
-      }
+    this.state = {
+      taskGroups: groups,
+      selectedTaskGroupName: groupName // the name of the selected taskGroup
+    };
 
-      Log.log('Hello fine people!');
+    Log.log("Hello fine people!");
   }
 
   render() {
+    const selectedTaskGroup = this.state.taskGroups.filter(
+      taskGroup => taskGroup.groupName === this.state.selectedTaskGroupName
+    )[0];
 
-    const selectedTaskGroup = this.state.taskGroups.filter((taskGroup) => taskGroup.groupName === this.state.selectedTaskGroupName)[0];
+    const samplePayload = `\n
+    {
+      id: 1,
+      group: "Purchases",
+      task: "Go to the bank",
+      dependencyIds: [],
+      completedAt: null,
+    },
+    {
+      id: 2,
+      group: "Purchases",
+      task: "Buy hammer",
+      dependencyIds: [1],
+      completedAt: null,
+    },
+    {
+      id: 3,
+      group: "Purchases",
+      task: "Buy wood",
+      dependencyIds: [1],
+      completedAt: null,
+    },
+    {
+      id: 4,
+      group: "Purchases",
+      task: "Buy nails",
+      dependencyIds: [1],
+      completedAt: null,
+    },
+    {
+      id: 5,
+      group: "Purchases",
+      task: "Buy paint",
+      dependencyIds: [1],
+      completedAt: null,
+    },
+    {
+      id: 6,
+      group: "Build Airplane",
+      task: "Hammer nails into wood",
+      dependencyIds: [2, 3, 4],
+      completedAt: null,
+    },
+    {
+      id: 7,
+      group: "Build Airplane",
+      task: "Paint wings",
+      dependencyIds: [6],
+      completedAt: null,
+    },
+    {
+      id: 8,
+      group: "Build Airplane",
+      task: "Have a snack",
+      dependencyIds: [11],
+      completedAt: null,
+    }`;
 
     return (
       <div className="container">
         <header className="App-header row">
           <div className="col">
             <img src={todo} className="App-logo" alt="logo" />
-            <span className="App-logo App-header-text text-uppercase">Semi-interesting ToDo List</span>
+            <span className="App-logo App-header-text text-uppercase">
+              Semi-interesting To-Do List
+            </span>
           </div>
         </header>
         <div className="row">
-            <div className="col-sm-5">
-              <h4>Things To Do</h4>
-              <TaskGroupList taskGroups={this.state.taskGroups} groupClick={this.onGroupClicked}/>
-            </div>
-            <div className="col-sm-1" />
-            <div className="col-sm-6">
-              <h4>Task Group</h4>
-              <TaskList tasks={selectedTaskGroup.groupTasks} taskClick={this.onTaskClicked} />
-            </div>
+          <div className="col-sm-5">
+            <h4>Things To Do</h4>
+            <TaskGroupList
+              taskGroups={this.state.taskGroups}
+              groupClick={this.onGroupClicked}
+            />
+          </div>
+          <div className="col-sm-1" />
+          <div className="col-sm-6">
+            <h4>Task Group</h4>
+            <TaskList
+              tasks={selectedTaskGroup.groupTasks}
+              taskClick={this.onTaskClicked}
+            />
+          </div>
         </div>
         <div className="instructions text-right">
-          <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          <button
+            id="instructionsBtn"
+            className="btn btn-primary"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapseExample"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
             View Instructions
           </button>
-          <div><small>(I was provided for this coding challenge)</small></div>
-          <div class="collapse" id="collapseExample">
-            <div class="well text-left">
-              <h5>Create a grouped task list with task dependencies</h5>
-            <ol class="list-group mb-5">
-              <li class="list-group-item">Build a task list UI in React using included design and SVG assets</li>
-              <li class="list-group-item"> The top level should show a list of task groups w/ # of tasks inside</li>
-              <li class="list-group-item">Clicking a group should display the list of all tasks for that group</li>
-              <li class="list-group-item">Tasks remain locked until all dependent tasks are complete</li>
-              <li class="list-group-item">Clicking a task should mark it as complete or incomplete, unless the task is locked</li>
-              <li class="list-group-item">Dependencies that do not point to a loaded task should be ignored</li>
-            </ol>
+          <div className="collapse" id="collapseExample">
+            <div className="text-left panel panel-info">
+              <div className="panel-heading">
+                The instructions below were provided for this coding exercise
+              </div>
+              <div className="panel-body">
+                <em>Create a grouped task list with task dependencies: </em>
+                <ul className="list-group mb-5">
+                  <li className="list-group-item">
+                    1. Build a task list UI in React using included design and
+                    SVG assets
+                  </li>
+                  <li className="list-group-item">
+                    2. The top level should show a list of task groups w/ # of
+                    tasks inside
+                  </li>
+                  <li className="list-group-item">
+                    3. Clicking a group should display the list of all tasks for
+                    that group
+                  </li>
+                  <li className="list-group-item">
+                    4. Tasks remain locked until all dependent tasks are
+                    complete
+                  </li>
+                  <li className="list-group-item">
+                    5. Clicking a task should mark it as complete or incomplete,
+                    unless the task is locked
+                  </li>
+                  <li className="list-group-item">
+                    6. Dependencies that do not point to a loaded task should be
+                    ignored
+                  </li>
+                </ul>
+                <div className="text-left">
+                  <pre>
+                    Sample Payload:
+                    {samplePayload}
+                  </pre>
+                </div>
+              </div>
             </div>
           </div>
         </div>
